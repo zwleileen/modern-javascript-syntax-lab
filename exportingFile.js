cc711ee6b3eb3cc30a97f40ba51869cdfdb4f6ec
eileen <zwleileen@gmail.com>
2025-01-13 13:42:40 +0800

    feat: practiced import and export

---
+++ exportingFile.js
@@ -0,0 +1,7 @@
+export default "Matt";
+export const computer = "MacBook Pro";
+
+const age = 43;
+const job = "programmer";
+
+export { age, job };
---
+++ importingFile.js
@@ -0,0 +1,4 @@
+import name, { computer, age, job } from "./exportingFile.js";
+
+console.log(name, computer, age, job);
+// Prints: 'Matt', 'MacBook Pro', 43, 'programmer'
